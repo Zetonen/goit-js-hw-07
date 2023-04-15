96f61f72b65de7aa0e4eeb4c1d5fa55efaa29226
Seven <graf.lancelot7@gmail.com>
2023-04-15 12:53:19 +0300

    first part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+const listItemsMakup = createListItemsMarkup(galleryItems);
+populateList(listItemsMakup);
+
+refs.galleryContainer.addEventListener("click", onImageClick);
+
+document.querySelector("a").addEventListener("click", (e) => {
+  blockStandartAction(e);
+
+  const newWindow = basicLightbox.create(document.querySelector("a"));
+
+  newWindow.show()
+});
+
+function createListItemsMarkup(elements) {
+  return elements
+    .map((element) => {
+      return `<li class="gallery__item">
+                <a href="${element.original}" class="gallery__link">
+                    <img class="gallery__image" src="${element.preview}" alt="${element.description}" data-source="${element.original}">
+                </a>
+                </li>`;
+    })
+    .join("");
+}
+
+function populateList(markup) {
+  refs.galleryContainer.innerHTML = markup;
+}
+
+function onImageClick(e) {
+  blockStandartAction(e);
+  const image = e.target;
+
+  if (image.nodeName !== "IMG") {
+    return;
+  }
+
+  const basicLightBoxImage =
+    basicLightbox.create(`    <img src="${image.dataset.source}" width="800" height="600">
+  `);
+  
+  basicLightBoxImage.show();
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      basicLightBoxImage.close();
+    }
+  });
+}
+
+function blockStandartAction(e) {
+  e.preventDefault();
+}
